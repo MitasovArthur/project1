0823fd68a54ccb2e6e59ce4788611b65804a5942
MitasovArthur <mitasovartur2@gmail.com>
2021-11-18 17:22:06 +0200

    Add

--- src/git.java
+++ src/git.java
@@ -2,6 +2,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("art");
         System.out.println("1231");
-
+        System.out.println("dafa");
+        int f = 45;
     }
 }
